2a6da38c10353e91e876d8aa320eee535b37db75
Truls Skeie <trulsske@iterate.no>
2017-05-04 08:27:32 +0200

    print error message

--- server/app.js
+++ server/app.js
@@ -50,23 +50,12 @@ app.use((req, res, next) => {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use((err, req, res, next) => {
-        console.log(err);
-
-        res.status(500);
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500);
-//   res.json('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+// if (app.get('env') === 'development') {
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500);
+});
+// }
 
 app.listen(app.get('port'), () => {
     console.log('Node app is running on port', app.get('port'));
